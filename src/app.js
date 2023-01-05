c85ae3997783ba2ddb841cea4dd3dbf102c3c637
Mateus <mateuskvasconcelos@gmail.com>
2023-01-05 19:06:07 -0300

    Update some bugs

--- src/app.js
+++ src/app.js
@@ -21,6 +21,11 @@ function checkImage(url) {
 app.post("/sign-up", (req, res) => {
   const user = req.body;
   if (
+    typeof req.body.username !== "string" ||
+    typeof req.body.avatar !== "string"
+  )
+    return res.status(400).send("Bad request, the payload is incorrect!");
+  if (
     Object.keys(user).length !== 2 ||
     !Object.keys(user).includes("username") ||
     !Object.keys(user).includes("avatar")
@@ -40,8 +45,16 @@ app.post("/sign-up", (req, res) => {
 app.get("/tweets", (req, res) => {
   // I could create a varibale to get the size of the page instead of set to 10
   let array = [];
-  let page = req.query.page ? parseInt(req.query.page) : 1;
-  if (!page >= 1) return res.status(400).send("Please enter a valid page!");
+  let page = Number(req.query.page);
+  if (req.query.page) {
+    if (Number.isNaN(page) || !Number.isInteger(page) || page < 1) {
+      return res.status(400).send("Please enter a valid page!");
+    } else {
+      page = Number(page);
+    }
+  } else {
+    page = 1;
+  }
   let max = tweets.length - (page - 1) * 10;
   let min = tweets.length - page * 10 >= 0 ? tweets.length - page * 10 : 0;
   const tweetsSlice = tweets.slice(min, max);
@@ -60,8 +73,8 @@ app.get("/tweets/:username", (req, res) => {
   let array = [];
   const username = req.params.username;
   let tweetsUser = tweets.filter((t) => t.username === username);
-  if (tweetsUser.length === 0)
-    return res.status(400).send("User doesn't have any tweet yet.");
+  // if (tweetsUser.length === 0)
+  //   return res.status(400).send("User doesn't have any tweet yet.");
   const avatar = users.filter((u) => u.username === tweetsUser[0].username)[0]
     .avatar;
   for (let i = tweetsUser.length - 1; i >= 0; i--) {
@@ -74,27 +87,11 @@ app.get("/tweets/:username", (req, res) => {
   res.status(200).send(array);
 });
 
-// app.post("/tweets", (req, res) => {
-//   const tweet = req.body;
-//   if (
-//     Object.keys(tweet).length !== 2 ||
-//     !Object.keys(tweet).includes("username") ||
-//     !Object.keys(tweet).includes("tweet")
-//   ) {
-//     return res.status(400).send("Bad request, the payload is incorrect!");
-//   } else if (Object.values(tweet).includes("")) {
-//     return res.status(400).send("All fields are mandatory!");
-//   }
-//   if (users.filter((u) => u.username === req.body.username).length === 0)
-//     return res.status(401).send("UNAUTHORIZED");
-//   tweets.push(tweet);
-//   res.status(201).send("OK");
-//   console.log(tweets);
-// });
-
 app.post("/tweets", (req, res) => {
   const username = req.header("user");
   const tweet = req.body;
+  if (typeof tweet.tweet !== "string")
+    return res.status(400).send("Bad request, the payload is incorrect!");
   if (
     Object.keys(tweet).length !== 1 ||
     !Object.keys(tweet).includes("tweet")
