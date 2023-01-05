abfff1c3b577b199f1cf89b55c27f1758e149e07
Mateus <mateuskvasconcelos@gmail.com>
2023-01-05 11:05:41 -0300

    Feature: Add some validations to the body in the POST methods for sign-up and tweet

--- src/app.js
+++ src/app.js
@@ -7,22 +7,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let users = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-  },
-];
-let tweets = [
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub",
-  },
-];
+let users = [];
+let tweets = [];
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
+  if (
+    Object.keys(user).length !== 2 ||
+    !Object.keys(user).includes("username") ||
+    !Object.keys(user).includes("avatar")
+  ) {
+    return res.status(400).send("Bad request, the payload is incorrect!");
+  }
   users.push(user);
   res.status(200).send("OK");
   console.log(users);
@@ -46,8 +42,15 @@ app.get("/tweets", (_, res) => {
 
 app.post("/tweets", (req, res) => {
   const tweet = req.body;
+  if (
+    Object.keys(tweet).length !== 2 ||
+    !Object.keys(tweet).includes("username") ||
+    !Object.keys(tweet).includes("tweet")
+  ) {
+    return res.status(400).send("Bad request, the payload is incorrect!");
+  }
   if (users.filter((u) => u.username === req.body.username).length === 0)
-    return res.send("UNAUTHORIZED");
+    return res.status(401).send("UNAUTHORIZED");
   tweets.push(tweet);
   res.status(200).send("OK");
   console.log(tweets);
