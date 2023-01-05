12894d1e601d446e232ee1bf4bfd7beb9d8463d3
Mateus <mateuskvasconcelos@gmail.com>
2023-01-05 10:52:24 -0300

    Feature: Method GET of tweets working

--- src/app.js
+++ src/app.js
@@ -2,22 +2,57 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 4000;
+const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Botão Sign");
-});
-
-let users = [];
+let users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+let tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+];
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
   users.push(user);
-  res.send("User is added to the database");
+  res.status(200).send("OK");
   console.log(users);
 });
 
-app.listen(PORT);
+app.get("/tweets", (_, res) => {
+  let array = [];
+  let stop = 0;
+  if (tweets.length > 10) {
+    stop = tweets.length - 11;
+  }
+  for (let i = tweets.length - 1; i > stop; i--) {
+    array.push({
+      username: tweets[i].username,
+      avatar: users.filter((u) => u.username === tweets[i].username)[0].avatar,
+      tweet: tweets[i].tweet,
+    });
+  }
+  res.status(200).send(array);
+});
+
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  if (users.filter((u) => u.username === req.body.username).length === 0)
+    return res.send("UNAUTHORIZED");
+  tweets.push(tweet);
+  res.status(200).send("OK");
+  console.log(tweets);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
