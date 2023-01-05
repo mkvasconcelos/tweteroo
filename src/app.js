4d7b1b4fd5bb10790f559739ec0b2273d87f13c1
Mateus <mateuskvasconcelos@gmail.com>
2023-01-05 07:44:30 -0300

    Feature: Method POST 'SIGN-UP' listen and get JSON sent

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const PORT = 4000;
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Botão Sign Up");
+  res.send("Botão Sign");
 });
+
+let users = [];
 
 app.post("/sign-up", (req, res) => {
-  // console.log(req.body); // the posted data
-  res.send(req);
+  const user = req.body;
+  users.push(user);
+  res.send("User is added to the database");
+  console.log(users);
 });
 
-// Configura o servidor para rodar na porta 4000
-app.listen(4000);
+app.listen(PORT);
